fc0cc018f3ca88fe0186972d9fa7c989f396a6f1
IB <ivica.budimir@campus02.edu.at>
2020-11-16 12:24:30 +0100

    Changes in appointment form
    change style of appointment form and add entity

--- motorcycleGang/src/app/appointment/appointment.component.ts
+++ motorcycleGang/src/app/appointment/appointment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-appointment',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./appointment.component.css']
 })
 export class AppointmentComponent implements OnInit {
+  email = new FormControl('', [Validators.required, Validators.email])
+  firstName = new FormControl('', [Validators.required])
+  lastName = new FormControl('', [Validators.required])
+
+  getErrorMessage() {
+    if (this.email.hasError('required') || this.firstName.hasError('required') || this.lastName.hasError('required') ) {
+      return 'You must enter a value'
+    }
+    return this.email.hasError('email') ? 'Not a valid email' : ''
+  }
 
   constructor() { }
 
---
+++ motorcycleGang/src/app/entities/appointment.ts
@@ -0,0 +1,11 @@
+export class Appointment {
+s
+    id: number;
+    firstName: string;
+    lastName: string;
+    phoneNumber: number;
+    email: string;
+    preferredDate: Date;
+    issue: string;
+
+  }
